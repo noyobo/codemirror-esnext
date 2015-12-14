f7831bc9e98e705795833e60142d4fda3e615c30
noyobo <nongyoubao@alibaba-inc.com>
2015-12-15 00:14:30 +0800

    token comment

---
+++ src/codemirror-esnext.js
@@ -0,0 +1,130 @@
+CodeMirror.defineMode('esnext', function(conf, parserConf) {
+  var ERRORCLASS = 'error';
+  var isOperatorChar = /[+\-*&%=<>!?|~^]/;
+
+  function readRegexp(stream) {
+    var escaped = false,
+      next, inSet = false;
+    while ((next = stream.next()) != null) {
+      if (!escaped) {
+        if (next == "/" && !inSet) return;
+        if (next == "[") inSet = true;
+        else if (inSet && next == "]") inSet = false;
+      }
+      escaped = !escaped && next == "\\";
+    }
+  }
+
+  // comment token 
+  function tokenComment(stream, state) {
+    var maybeEnd = false;
+    var ch;
+    var pch = stream.peek()
+    while (ch = stream.next()) {
+      console.log('a', ch, 's', pch, stream.string);
+      if (ch === '@') {
+        state.scope.type = 'comment';
+        state.tokenize = tokenCommentProperty;
+        break;
+      }
+      if (ch === '/' && maybeEnd) {
+        state.tokenize = tokenBase;
+        state.scope.type = 'sol';
+        break;
+      }
+      maybeEnd = (ch === '*');
+      console.log(maybeEnd);
+    }
+    return 'comment';
+  }
+  // super jsdoc defined property multi line comments
+  function tokenCommentProperty(stream, state) {
+    while (stream.eat(/[\S]/)) {
+      if (stream.match(/[\:\ ]/) || stream.eol()) {
+        state.tokenize = tokenBase;
+        break;
+      }
+    }
+    return 'def';
+  }
+
+  function tokenBase(stream, state) {
+    if (stream.sol() && state.scope.type === 'sol') {
+      // all space class indented;
+      // eat space
+      if (stream.eatSpace()) {
+        var lineOffset = stream.indentation();
+        if (lineOffset) {
+          return 'indent';
+        }
+        return null;
+      }
+    }
+
+    var ch = stream.next();
+    if (state.scope.type === 'comment') {
+      state.tokenize = tokenComment;
+      return state.tokenize(stream, state);
+    }
+    if (ch === '/') {
+      if (stream.eat('*')) {
+        state.tokenize = tokenComment;
+        state.scope.type = 'comment';
+        return tokenComment(stream, state);
+      } else if (stream.eat('/')) {
+        stream.skipToEnd();
+        return 'comment';
+      } else if (/^(?:operator|sol|keyword c|case|new|[\[{}\(,;:])$/.test(
+          state.scope.type)) {
+        readRegexp(stream);
+        stream.match(/^\b(([gimyu])(?![gimyu]*\2))+\b/);
+        return 'regexp';
+      } else {
+        stream.eatWhile(isOperatorChar);
+        return 'operator';
+      }
+    }
+    return ERRORCLASS;
+  }
+
+  var esnext = {
+    startState: function(basecolumn) {
+      return {
+        tokenize: tokenBase,
+        scope: {
+          offset: basecolumn || 0,
+          type: 'sol',
+          offset: 0
+        },
+        prop: false,
+        indented: 0
+      };
+    },
+
+    token: function(stream, state) {
+
+      return state.tokenize(stream, state);
+    },
+
+    blockCommentStart: '/*',
+    blockCommentEnd: '*/',
+    lineComment: '//',
+    fold: 'brace',
+    closeBrackets: '()[]{}\'\'""``',
+  };
+
+  return esnext;
+});
+
+
+CodeMirror.defineMIME('text/javascript', 'esnext');
+CodeMirror.defineMIME('text/ecmascript', 'esnext');
+CodeMirror.defineMIME('text/babel', 'esnext');
+CodeMirror.defineMIME('text/jsx', 'esnext');
+CodeMirror.defineMIME('application/javascript', 'esnext');
+CodeMirror.defineMIME('application/x-javascript', 'esnext');
+CodeMirror.defineMIME('application/ecmascript', 'esnext');
+CodeMirror.defineMIME('application/json', {
+  name: 'esnext',
+  json: true
+});
--- src/index.js
+++
@@ -1,2 +0,0 @@
-var CodeMirror = require('codemirror');
-

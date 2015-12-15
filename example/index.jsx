8b9f2e6bc36d6a638c746d3b72c20fea99f7ba11
noyobo <nongyoubao@alibaba-inc.com>
2015-12-16 01:20:44 +0800

    quasi string

--- example/index.jsx
+++ example/index.jsx
@@ -1,20 +1,18 @@
+// asfsdfsdfsfsfsfas 
+/[adfa]/.test('a')
 /* eslint no-console:0 */
 /**
- * @author asfdsfds
+ * @author
  */
 import 'rc-time-picker/assets/index.css';
 
+`sfsdfsfsafs${word}`
+`
+ There are <b>${basket.count}</b> items 
+in your basket, <em>${basket.onSale}</em> 
+are on sale! 
+`
 import React from 'react';
-import ReactDom from 'react-dom';
-
-import GregorianCalendar from 'gregorian-calendar';
-import DateTimeFormat from 'gregorian-calendar-format';
-import zhCn from 'gregorian-calendar/lib/locale/zh_CN';
-
-import TimePicker from 'rc-time-picker';
-import TimePickerLocale from 'rc-time-picker/lib/locale/zh_CN';
-
-const formatter = new DateTimeFormat('HH:mm:ss');
 
 const now = new GregorianCalendar(zhCn);
 now.setTime(Date.now());
@@ -26,116 +24,13 @@ const App = React.createClass({
     };
   },
   handleValueChange(value) {
-    console.log(value && formatter.format(value));
     this.setState({ value });
   },
   render() {
     return (
       <div>
-        <TimePicker formatter={formatter} locale={TimePickerLocale}
-              defaultValue={now} />
-        <TimePicker formatter={formatter} locale={TimePickerLocale}
-              value={this.state.value}
-              onChange={this.handleValueChange}/>
+        
       </div>
     );
   },
 });
-
-ReactDom.render(
-  <App />,
-  document.getElementById('__react-content')
-);
-
-
-import React, { Component } from 'react';
-import {PropTypes} from 'react-router';
-
-
-import Loading from '../../components/loading/';
-import message from '../../components/message/';
-import Alert from '../../components/alert/';
-
-export default class Task extends Component {
-
-  constructor() {
-    super(...arguments);
-
-    this.state = {
-      alert: false,
-      user: false,
-    };
-  }
-
-  componentWillMount() {
-    const location = this.props.location;
-    const params = this.props.params;
-
-    if (location.query.repo) {
-      this.rewriteFormRepo({
-        repo: location.query.repo,
-      });
-    } else if (location.query.url) {
-      this.rewriteFormUrl({
-        url: location.query.url,
-      });
-    } else if (params && params.taskId && parseInt(params.taskId, 10) > 0) {
-      base.get(API.detail({
-        id: params.taskId,
-      }))
-      .then((res) => {
-        if (res.status !== 'success') {
-          this.setState({
-            alert: true,
-          });
-        } else {
-          const task = res.task;
-          if (params.historyId && parseInt(params.historyId, 10) > 0) {
-            this.context.history.replaceState(null, `/group/${task.projectId}/task/${task.id}/history/${params.historyId}`);
-          } else {
-            this.context.history.replaceState(null, `/group/${task.projectId}/task/${task.id}`);
-          }
-        }
-      });
-    } else {
-      this.context.history.replaceState(null, '/group/my');
-    }
-  }
-
-
-  rewriteFormRepo = (query) => {
-    base.get(API.tasks, query)
-    .then((res) => {
-      if (res.status !== 'success') {
-        message.show(res.status, res.message);
-      } else {
-        if (res.tasks && res.tasks.length !== 0) {
-          const task = res.tasks[0];
-          this.context.history.replaceState(null,
-            '/group/' + task.projectId + '/task/' + task.id);
-        } else {
-          this.setState({
-            alert: true,
-            show: false,
-            message: '暂无 ' + (query.repo || query.url) + ' 的相关测试结果',
-          });
-        }
-      }
-    });
-  }
-
-  render() {
-    let renderElement = <Loading />;
-
-    if (this.state.alert) {
-      renderElement = <Alert message="未找到相关内容, 请重新输入" />;
-    }
-
-    return (
-      renderElement
-    );
-  }
-}
-
-
-Task.contextTypes = { history: PropTypes.history };
--- src/codemirror-esnext.js
+++ src/codemirror-esnext.js
@@ -1,6 +1,27 @@
-CodeMirror.defineMode('esnext', function(conf, parserConf) {
+CodeMirror.defineMode('esnext', function(config, parserConf) {
   var ERRORCLASS = 'error';
+  var indentUnit = config.indentUnit
   var isOperatorChar = /[+\-*&%=<>!?|~^]/;
+
+  // Parser
+
+  var atomicTypes = {
+    "atom": true,
+    "number": true,
+    "variable": true,
+    "string": true,
+    "regexp": true,
+    "this": true
+  };
+
+  function JSLexical(indented, column, type, align, prev, info) {
+    this.indented = indented;
+    this.column = column;
+    this.type = type;
+    this.prev = prev;
+    this.info = info;
+    if (align != null) this.align = align;
+  }
 
   function readRegexp(stream) {
     var escaped = false;
@@ -21,6 +42,74 @@ CodeMirror.defineMode('esnext', function(conf, parserConf) {
     }
   }
 
+  function tokenFactory(delimiter, singleline, outclass) {
+    return function(stream, state) {
+      while (!stream.eol()) {
+        stream.eatWhile(/[^'"\/\\]/);
+        if (stream.eat("\\")) {
+          stream.next();
+          if (singleline && stream.eol()) {
+            return outclass;
+          }
+        } else if (stream.match(delimiter)) {
+          state.tokenize = tokenBase;
+          return outclass;
+        } else {
+          stream.eat(/['"\/]/);
+        }
+      }
+      if (singleline) {
+        if (parserConf.singleLineStringErrors) {
+          outclass = ERRORCLASS;
+        } else {
+          state.tokenize = tokenBase;
+        }
+      }
+      return outclass;
+    };
+  }
+
+  function tokenQuasiVariable(stream, state) {
+    while (ch = stream.next()) {
+      if (ch === '}') {
+        state.tokenize = tokenQuasi;
+        return 'keyword'
+        break;
+      }
+      return 'variable'
+    }
+  }
+
+  function tokenQuasi(stream, state) {
+    var maybeEnd = false;
+    while (!stream.eol()) {
+      ch = stream.next();
+      if (ch === '$' && stream.eat('{')) {
+        state.scope.type = 'quasi';
+        state.tokenize = tokenQuasiVariable;
+        return 'string-2'
+      }
+      if (ch === '`') {
+        state.scope.type = 'normal';
+        state.tokenize = tokenBase;
+        return 'string-2'
+      }
+      return 'string'
+    }
+  }
+
+  // super jsdoc defined property multi line comments
+  function tokenCommentProperty(stream, state) {
+    while (ch = stream.peek()) {
+      if (ch === '*' || ch === ':' || ch === ' ' || stream.eol()) {
+        state.tokenize = tokenComment;
+        break;
+      }
+      stream.next();
+    }
+    return 'comment def';
+  }
+
   // comment token
   function tokenComment(stream, state) {
     var maybeEnd = false;
@@ -33,26 +122,16 @@ CodeMirror.defineMode('esnext', function(conf, parserConf) {
       }
       if (ch === '/' && maybeEnd) {
         state.tokenize = tokenBase;
-        state.scope.type = 'sol';
+        state.scope.type = 'normal';
         break;
       }
       maybeEnd = ch === '*';
     }
     return 'comment';
   }
-  // super jsdoc defined property multi line comments
-  function tokenCommentProperty(stream, state) {
-    while (stream.eat(/[\S]/)) {
-      if (stream.match(/[\:\ ]/) || stream.eol()) {
-        state.tokenize = tokenBase;
-        break;
-      }
-    }
-    return 'def';
-  }
 
   function tokenBase(stream, state) {
-    if (stream.sol() && state.scope.type === 'sol') {
+    if (stream.sol() && state.scope.type === 'normal') {
       // all space class indented;
       // eat space
       if (stream.eatSpace()) {
@@ -69,7 +148,13 @@ CodeMirror.defineMode('esnext', function(conf, parserConf) {
       state.tokenize = tokenComment;
       return state.tokenize(stream, state);
     }
-    if (ch === '/') {
+    if (ch == '"' || ch == "'") {
+      state.tokenize = tokenFactory(stream.current(), false, "string");
+      return state.tokenize(stream, state);
+    } else if (ch == "`") {
+      state.tokenize = tokenQuasi;
+      return 'string-2';
+    } else if (ch === '/') {
       if (stream.eat('*')) {
         state.tokenize = tokenComment;
         state.scope.type = 'comment';
@@ -77,7 +162,7 @@ CodeMirror.defineMode('esnext', function(conf, parserConf) {
       } else if (stream.eat('/')) {
         stream.skipToEnd();
         return 'comment';
-      } else if (/^(?:operator|sol|keyword c|case|new|[\[{}\(,;:])$/.test(
+      } else if (/^(?:operator|normal|keyword c|case|new|[\[{}\(,;:])$/.test(
           state.scope.type)) {
         readRegexp(stream);
         stream.match(/^\b(([gimyu])(?![gimyu]*\2))+\b/);
@@ -94,9 +179,13 @@ CodeMirror.defineMode('esnext', function(conf, parserConf) {
     startState: function(basecolumn) {
       return {
         tokenize: tokenBase,
+        lexical: new JSLexical((basecolumn || 0) - indentUnit, 0, "block", false),
         scope: {
           offset: basecolumn || 0,
-          type: 'sol'
+          type: 'normal',
+          prev: null,
+          align: false,
+          content: null
         },
         prop: false,
         indented: 0
@@ -104,15 +193,16 @@ CodeMirror.defineMode('esnext', function(conf, parserConf) {
     },
 
     token: function(stream, state) {
-
       return state.tokenize(stream, state);
     },
 
+    closeBrackets: '()[]{}\'\'""``',
+    electricInput: /^\s*(?:case .*?:|default:|\{|\})$/,
     blockCommentStart: '/*',
     blockCommentEnd: '*/',
+    blockCommentContinue: ' * ',
     lineComment: '//',
     fold: 'brace',
-    closeBrackets: '()[]{}\'\'""``',
   };
 
   return esnext;
